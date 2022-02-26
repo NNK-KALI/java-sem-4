7fef5984356d478ddd940a47407b491f4cab402d
neerajnangiredddy <nnangire@gitam.in>
2022-02-26 12:34:17 +0530

    # 15

---
+++ programs/DegreeToRadian.java
@@ -0,0 +1,17 @@
+// wap to convert the given degres to radian using math functions
+
+import java.io.*;
+
+public class DegreeToRadian
+{
+	public static void main(String args[]) throws IOException
+	{
+		InputStreamReader inp = new InputStreamReader(System.in);
+		BufferedReader read = new BufferedReader(inp);
+		System.out.println("Enter Degrees");
+		float deg = Float.parseFloat(read.readLine());
+		float rad = (float)(deg * Math.PI / 180);
+		System.out.println(rad + "Rad");
+	}
+}
+
