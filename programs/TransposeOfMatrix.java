0c989985ee3bb606eb454f9d79d2c22dfbca2a05
neerajnangiredddy <nnangire@gitam.in>
2022-02-26 12:34:17 +0530

    # 17

---
+++ programs/TransposeOfMatrix.java
@@ -0,0 +1,50 @@
+/*
+    17) WAP to find the Transose of Given matrix
+*/
+
+import java.util.Scanner;
+
+public class TransposeOfMatrix
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Enter no of rows ");
+        int m = read.nextInt();
+        System.out.println("Enter no of columns ");
+        int n = read.nextInt();
+
+        int arr[][] = new int[m][n];
+
+        System.out.println("Enter array elements ");
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                System.out.print("A [" + i + "][" + j + "] ");
+                arr[i][j] = read.nextInt();
+            }
+        }
+
+        System.out.println("Original Matrix ");
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Tanspose of matrix ");
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                System.out.print(arr[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
