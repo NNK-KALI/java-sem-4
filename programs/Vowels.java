a625c428518ea416499e4d47a4e60b089fa9ea5c
neerajnangiredddy <nnangire@gitam.in>
2022-03-01 23:39:58 +0530

    # 25

---
+++ programs/Vowels.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Vowels
+{
+    public static void  main(String args[])
+    {
+        Scanner scan = new Scanner(System.in);
+
+        char vowels[] = {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'};
+        System.out.print("Enter statement: ");
+        String str = scan.nextLine();
+        scan.close();
+        int count = 0;
+        for(int i=0; i<str.length(); i++)
+        {
+            for(int j=0; j<vowels.length; j++)
+            {
+                if(str.charAt(i) == vowels[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        
+        System.out.println("No of vowels = " + count);
+
+    }
+}
