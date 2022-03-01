78e8215fd8d6640f06c6416744b7ceaca59f5152
neerajnangiredddy <nnangire@gitam.in>
2022-03-01 23:40:11 +0530

    # 26

---
+++ programs/NoOfWordsInAStatement.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class NoOfWordsInAStatement
+{
+    public static void main(String args[])
+    {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter the statement: ");
+        String str = read.nextLine().trim();
+        read.close();
+        int count = 0;
+        
+        // str = str.replaceAll("\\s+", " ");  using regex
+        // for(int i=0; i<str.length(); i++)
+        // {
+        //     if(str.charAt(i) == 32)
+        //     {count++;}
+        // }
+        
+        for(int i=0; i<str.length(); i++)
+        {
+        	if(str.charAt(i) == 32 && str.charAt(i+1) != 32)
+        	{
+        		count++;
+        	}
+        }
+        count++;
+        System.out.println("No of words = " + count);
+    }
+}
